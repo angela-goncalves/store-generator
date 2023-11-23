79f1e1571b61f97901c15f52ef109cefac8fde38
Angela <angelakgo20@gmail.com>
2023-11-23 13:02:06 -0300

    create add_collections and its error page

---
+++ app/add_collections/error.tsx
@@ -0,0 +1,26 @@
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2>Something went wrong!</h2>
+      {error.message}
+      <button
+        className="m-4 p-4 border rounded-lg border-gray-300"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }>
+        Try again
+      </button>
+    </div>
+  );
+}
