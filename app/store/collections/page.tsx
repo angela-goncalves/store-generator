fbb0322b83bfe4e1c8a42fc1a4307e09efca8c94
Angela <angelakgo20@gmail.com>
2023-12-15 16:37:39 -0300

    delete add-collections page

--- app/store/collections/page.tsx
+++ app/store/collections/page.tsx
@@ -3,13 +3,10 @@ import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 import React from "react";
 import Link from "next/link";
-import { PencilLineIcon, Plus } from "lucide-react";
-import { Separator } from "@/components/ui/separator";
-import { capitalizeFirstLetter } from "@/lib/uppercase";
-import DeleteCollection from "@/components/forms/deleteCollection";
 import BackButton from "@/components/BackButton";
 import Image from "next/image";
 import collectionImage from "@/app/public/categories.png";
+import AddCollectionsForm from "@/components/forms/addCollectionsForm";
 
 export default async function Collections({
   searchParams,
@@ -38,49 +35,17 @@ export default async function Collections({
           query: { id: searchParams.id },
         }}
       />
-      <div className="w-full max-w-[800px] flex flex-col h-full gap-6">
+      <div className="w-full max-w-[800px] flex flex-col h-full">
         <div className="flex justify-between">
-          <h1 className="text-secondary font-bold text-3xl">Collections</h1>
-          <Link
-            href={{
-              pathname: "/store/collections/add-collections",
-              query: { id: storeId },
-            }}
-            className="flex border items-center border-secondary rounded-lg text-sm p-2 my-4 text-center gap-2">
-            <Plus className="w-4 " />
-            <p className="text-primary-foreground ">Add new collection</p>
-          </Link>
+          <h1 className="text-secondary font-bold text-3xl mt-10">
+            Collections
+          </h1>
         </div>
         {dataCollections.length > 0 ? (
-          <ul className="flex flex-col gap-4 mt-">
-            <h3 className="text-2xl">Title</h3>
-            {dataCollections.map((item) => (
-              <div key={item.id}>
-                <li className="flex justify-between gap-4">
-                  <h3>{capitalizeFirstLetter(item.name)}</h3>
-                  <div className="flex gap-2 items-center">
-                    <Link
-                      href={{
-                        pathname: `/store/collections/edit-collection`,
-                        query: {
-                          id: storeId,
-                          collectionId: `${item.id}`,
-                          collectionTitle: `${item.name}`,
-                          collectionDescription: `${item.description}`,
-                        },
-                      }}>
-                      <PencilLineIcon className="mr-2 h-4 w-4" />
-                    </Link>
-                    <DeleteCollection
-                      collectionId={item.id}
-                      storeId={storeId}
-                    />
-                  </div>
-                </li>
-                <Separator className="bg-neutral-dark" />
-              </div>
-            ))}
-          </ul>
+          <AddCollectionsForm
+            storeId={searchParams.id}
+            dataCollections={dataCollections}
+          />
         ) : (
           <div className="h-full mt-20">
             <div className="flex justify-center self-center bg-white p-6 rounded-lg gap-10">
--- components/forms/addCollectionsForm.tsx
+++ components/forms/addCollectionsForm.tsx
@@ -5,17 +5,38 @@ import { Input } from "@/components/ui/input";
 import { handleInsertCollections } from "@/lib/insertSupabase";
 import { Button } from "../ui/button";
 import { v4 as uuidv4 } from "uuid";
+import { PencilLineIcon, Plus } from "lucide-react";
+import { capitalizeFirstLetter } from "@/lib/uppercase";
+import Link from "next/link";
+import { Separator } from "../ui/separator";
+import DeleteDialog from "../DeleteDialog";
 
 type FormDataType = {
   name: string;
   id: string;
 };
+interface IDataCollections {
+  created_at: string;
+  description: string | null;
+  id: string;
+  image: string | null;
+  name: string | null;
+  store_id: string | null;
+}
 
-export default function AddCollectionsForm({ storeId }: { storeId: string }) {
+export default function AddCollectionsForm({
+  storeId,
+  dataCollections,
+}: {
+  storeId: string;
+  dataCollections: IDataCollections[];
+}) {
   const myUUID = uuidv4();
   const [collectionslist, setCollectionslist] = useState<FormDataType[]>([
     { name: "", id: myUUID },
   ]);
+
+  const [addCollection, setAddCollection] = useState(false);
 
   const handleInputChange = (index: number, field: "name", value: string) => {
     const updatedCollection = [...collectionslist];
@@ -26,62 +47,104 @@ export default function AddCollectionsForm({ storeId }: { storeId: string }) {
   const addNewCollection = () => {
     setCollectionslist([...collectionslist, { name: "", id: myUUID }]);
   };
+
   const deleteCollectionAdded = (idcollectionAdded: string) => {
-    const collectionslistCopy = [...collectionslist];
-    const deletedCollections = collectionslistCopy.filter(
-      (item) => item.id === idcollectionAdded
+    const deletedCollections = collectionslist.filter(
+      (item) => item.id !== idcollectionAdded
     );
     setCollectionslist(deletedCollections);
   };
+
   return (
-    <div className="w-1/2 max-w-[500px]">
-      <form
-        action={() => handleInsertCollections(collectionslist, storeId)}
-        className="flex flex-col"
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault();
-          }
-        }}>
-        {collectionslist.map((item, index) => (
-          <div
-            key={item.id}
-            className="flex justify-between mt-6 items-center gap-4">
-            <Input
-              type="text"
-              name={item.name}
-              className=" w-full"
-              value={item.name}
-              onChange={(e) => handleInputChange(index, "name", e.target.value)}
-              onKeyDown={(e) => {
-                if (e.code === "Enter") return addNewCollection();
-              }}
-              placeholder="Name of the collection"
-            />
-            {collectionslist.length > 1 && (
+    <div className="w-full flex flex-col mb-10">
+      <Button
+        variant="ghost"
+        onClick={() => setAddCollection(!addCollection)}
+        className="flex border self-end items-center border-secondary rounded-lg text-sm p-2 mb-4 text-center gap-2">
+        <Plus className="w-4 " />
+        <p className="text-primary-foreground ">Add new collection</p>
+      </Button>
+      <ul className="flex flex-col gap-4 mt-">
+        <h3 className="text-2xl">Title</h3>
+        {dataCollections.map((item) => (
+          <div key={item.id}>
+            <li className="flex justify-between gap-4">
+              <h3>{capitalizeFirstLetter(item.name)}</h3>
+              <div className="flex gap-2 items-center">
+                <Link
+                  href={{
+                    pathname: `/store/collections/edit-collection`,
+                    query: {
+                      id: storeId,
+                      collectionId: `${item.id}`,
+                      collectionTitle: `${item.name}`,
+                      collectionDescription: `${item.description}`,
+                    },
+                  }}>
+                  <PencilLineIcon className="mr-2 h-4 w-4" />
+                </Link>
+                <DeleteDialog collectionId={item.id} storeId={storeId} />
+              </div>
+            </li>
+            <Separator className="bg-neutral-dark" />
+          </div>
+        ))}
+      </ul>
+      {addCollection && (
+        <form
+          action={() => handleInsertCollections(collectionslist, storeId)}
+          className="flex flex-col mt-2"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+            }
+          }}>
+          {collectionslist.map((item, index) => (
+            <div
+              key={item.id}
+              className="flex justify-between mt-6 items-center gap-4">
+              <Input
+                type="text"
+                name={item.name}
+                className=" w-full"
+                value={item.name}
+                onChange={(e) =>
+                  handleInputChange(index, "name", e.target.value)
+                }
+                onKeyDown={(e) => {
+                  if (e.code === "Enter") return addNewCollection();
+                }}
+                placeholder="Name of the collection"
+              />
               <Button
                 variant="outline"
                 type="button"
                 className="w-max px-6 rounded-lg py-4 self-start bg-transparent border-primary"
-                onClick={() => deleteCollectionAdded(item.id)}>
+                onClick={() =>
+                  collectionslist.length > 1
+                    ? deleteCollectionAdded(item.id)
+                    : setAddCollection(false)
+                }>
                 x
               </Button>
-            )}
+            </div>
+          ))}
+          <div className="flex justify-between">
+            <Button
+              type="button"
+              className="my-6 px-6 rounded-lg py-4 self-start bg-transparent border-primary"
+              onClick={addNewCollection}
+              variant="outline">
+              <Plus className="w-4 " />
+            </Button>
+            <Button
+              className="border max-w-[200px] self-end rounded-lg px-6 py-4 my-6"
+              type="submit">
+              Save
+            </Button>
           </div>
-        ))}
-        <Button
-          type="button"
-          className="my-6 px-6 rounded-lg py-4 self-start bg-transparent border-primary"
-          onClick={addNewCollection}
-          variant="outline">
-          add one more
-        </Button>
-        <Button
-          className="border max-w-[200px] self-end rounded-lg px-6 py-4 my-6"
-          type="submit">
-          submit
-        </Button>
-      </form>
+        </form>
+      )}
     </div>
   );
 }
