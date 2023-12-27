8a8ef2a2a4821745e8611ccbe4a62570e2f22012
Angela <angelakgo20@gmail.com>
2023-12-27 17:45:16 -0300

    change add-products page to get a better understanding of the props sended to the form

--- app/store/products/add-products/page.tsx
+++ app/store/products/add-products/page.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import { createClient } from "@/utils/supabase/server";
-import { cookies } from "next/headers";
 import AddProductsForm from "@/components/forms/addProductsForm";
-import { redirect } from "next/navigation";
 import BackButton from "@/components/BackButton";
-
-interface IProducts {
-  created_at: string;
-  description: string | null;
-  id: string;
-  image: string | null;
-  name: string | null;
-  store_id: string | null;
-}
+import { getCollectionsOfStore, getProductsToEdit } from "@/lib/action/getData";
 
 export default async function FormAddProducts({
   searchParams,
@@ -20,23 +9,13 @@ export default async function FormAddProducts({
   searchParams: {
     id: string;
     productId: string;
-    productName: string;
-    productDescription: string;
-    productPrice: string;
-    productImage: string;
-    collectionId: string;
   };
 }) {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  const { data, error } = await supabase
-    .from("collections")
-    .select()
-    .eq("store_id", searchParams.id);
+  const collections = await getCollectionsOfStore(searchParams.id);
 
-  if (data === null || error !== null) {
-    redirect(`/store/products/add-products?id=${searchParams.id}`);
-  }
+  const productToEdit = searchParams.productId
+    ? await getProductsToEdit(searchParams.productId)
+    : [];
 
   return (
     <div className="my-10 mx-2 w-full flex flex-col items-center ">
@@ -48,7 +27,11 @@ export default async function FormAddProducts({
       />
       <h1 className="text-2xl mb-12">Add or edit your products</h1>
       <h2>Select the collection you want to be related to your products</h2>
-      <AddProductsForm dataCollections={data} searchParams={searchParams} />
+      <AddProductsForm
+        dataCollections={collections || []}
+        productToEdit={productToEdit.length > 0 ? productToEdit[0] : {}}
+        storeId={searchParams.id}
+      />
     </div>
   );
 }
