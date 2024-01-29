178db106e7c390c977fed3729b94241ace9dc082
Angela <angelakgo20@gmail.com>
2024-01-29 19:38:27 -0300

    changes components of menubar in Navbar

--- components/store/navbar.tsx
+++ components/store/navbar.tsx
@@ -1,16 +1,16 @@
+"use client";
 import Link from "next/link";
-import React from "react";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
-import { SearchIcon } from "lucide-react";
+import React, { useState } from "react";
+import { MenuIcon, SearchIcon } from "lucide-react";
 import { Input } from "../ui/input";
 import { capitalizeFirstLetter } from "@/lib/uppercase";
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger,
+} from "../ui/menubar";
 
 interface ICollections {
   created_at: string;
@@ -23,47 +23,53 @@ interface ICollections {
 
 interface NavbarProps {
   dataCollections: ICollections[];
-  nameStore: string;
 }
 
-export default function Navbar({ dataCollections, nameStore }: NavbarProps) {
+export default function Navbar({ dataCollections }: NavbarProps) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="p-4 w-full grid grid-cols-3 justify-between items-center">
-      <div className="flex items-center">
-        <div>
-          {dataCollections.length > 0 ? (
-            <NavigationMenu>
-              <NavigationMenuList>
-                <ul className="flex gap-4">
-                  <NavigationMenuItem>
-                    <NavigationMenuTrigger className="bg-transparent hover:bg-transparent" />
-                    <NavigationMenuContent className="min-w-[200px] px-6 pt-4 pb-5 bg-white">
-                      <NavigationMenuLink>
-                        {dataCollections.map((item) => (
-                          <Link href="#" key={item.id} className="text-sm">
-                            <p className=" w-32">
-                              {capitalizeFirstLetter(item.name || "")}
-                            </p>
-                          </Link>
-                        ))}
-                      </NavigationMenuLink>
-                    </NavigationMenuContent>
-                  </NavigationMenuItem>
-                </ul>
-              </NavigationMenuList>
-            </NavigationMenu>
-          ) : (
-            <h3>Navbar</h3>
-          )}
-        </div>
-        <div className="relative">
-          <Input type="text" className="w-full max-w-xs h-8 rounded-full" />
-          <SearchIcon className="absolute top-1 right-2 w-5" />
-        </div>
+    <div className="p-4 w-full flex items-center">
+      <Menubar className="mr-6 bg-transparent border-none">
+        <MenubarMenu>
+          <MenubarTrigger
+            className="data-[state=open]:bg-transparent focus:bg-transparent"
+            onClick={toggleMenu}>
+            <MenuIcon
+              className={`ml-1 h-6 w-6 transition duration-150 ${
+                isMenuOpen ? "rotate-180" : ""
+              }`}
+              aria-hidden="true"
+            />
+          </MenubarTrigger>
+          <MenubarContent className="">
+            {dataCollections.length > 0 ? (
+              <div>
+                {dataCollections.map((item) => (
+                  <MenubarItem key={item.id}>
+                    <Link href="#" className="text-sm">
+                      <p className=" w-32">
+                        {capitalizeFirstLetter(item.name || "")}
+                      </p>
+                    </Link>
+                  </MenubarItem>
+                ))}
+              </div>
+            ) : (
+              <MenubarItem>
+                <h3>Categories</h3>
+              </MenubarItem>
+            )}
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+      <div className="relative w-full max-w-xs">
+        <Input type="text" className="h-8 rounded-full" />
+        <SearchIcon className="absolute top-1 right-2 w-5" />
       </div>
-      <Link href={`/${nameStore}`} className="justify-self-center text-lg">
-        {nameStore || "Name of your store"}
-      </Link>
     </div>
   );
 }
