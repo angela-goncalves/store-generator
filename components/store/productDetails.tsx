b4a0b1f843765d43c8ed8703ff53f7454fed9ab6
Angela <angelakgo20@gmail.com>
2024-03-14 14:10:06 -0300

    conflicts to merge

--- components/store/productDetails.tsx
+++ components/store/productDetails.tsx
@@ -5,23 +5,15 @@ import { Button } from "../ui/button";
 import { capitalizeFirstLetter } from "@/lib/uppercase";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { onSubmitProductToShoppingBag } from "@/lib/action/cookies";
+import Link from "next/link";
 
-interface IProduct {
-  id: string;
-  created_at: string;
-  description: string;
-  image: string;
-  name: string;
-  price: string;
-  store_id: string;
-}
 interface IAttributes {
   id: string;
   name: string;
   children_values: string[];
 }
 interface IProductComponent {
-  productData: IProduct;
+  productData: Products;
   attributes: any[];
   storeName: string;
 }
@@ -51,57 +43,61 @@ export default function ProductDetails({
       action={() =>
         onSubmitProductToShoppingBag(attributesForm, productData, 1, storeName)
       }>
-      <div className="max-w-xs flex">
-        <div className="flex flex-col self-end ml-16">
-          <h3>Composition</h3>
-          <p className="text-sm mt-3">
-            We work with monitoring programs to guarantee compliance with the
-            social, environmental, and health and safety standards of our
-            products. To assess its compliance, we have developed an audit
-            program and plans for continual improvement.
-          </p>
-        </div>
-      </div>
-      <div className="flex flex-col mt-6">
-        <h2 className="text-3xl">{productData.name.toUpperCase()}</h2>
-        <div className="flex gap-4 mt-14 bg-secondary p-4">
-          <img
-            className="max-w-[300px]"
-            src={productData.image}
-            alt={`${productData.name} image`}
-          />
-        </div>
-      </div>
-      <div className="flex flex-col justify-center gap-14">
-        <div className="p-2 max-w-xs h-min">
-          <p className="text-sm">
-            {capitalizeFirstLetter(productData.description || "")}
-          </p>
-        </div>
-        <p className="text-2xl">${productData.price}</p>
-        {attributes.map((item: IAttributes) => (
-          <div key={item.id}>
-            <RadioGroup
-              name={item.name}
-              onValueChange={(e) => onChange(e, item.name)}>
-              <p className="font-bold">{item.name}</p>
-              <div className="flex gap-4">
-                {item.children_values.map((ele: any) => (
-                  <label
-                    key={ele}
-                    htmlFor={ele}
-                    className="flex gap-2 items-center">
-                    <RadioGroupItem value={ele} id={ele} required />
-                    <p>{ele}</p>
-                  </label>
-                ))}
-              </div>
-            </RadioGroup>
+      <div>
+        <div className="max-w-xs flex">
+          <div className="flex flex-col self-end ml-16">
+            <h3>Composition</h3>
+            <p className="text-sm mt-3">
+              We work with monitoring programs to guarantee compliance with the
+              social, environmental, and health and safety standards of our
+              products. To assess its compliance, we have developed an audit
+              program and plans for continual improvement.
+            </p>
           </div>
-        ))}
-        <Button variant="outline" type="submit">
-          Add to Bag
-        </Button>
+        </div>
+        <div className="flex flex-col mt-6">
+          <h2 className="text-3xl">{productData.name?.toUpperCase()}</h2>
+          <div className="flex gap-4 mt-14 bg-secondary p-4">
+            {productData.images?.map((item) => (
+              <img
+                className="max-w-[300px]"
+                src={typeof item === "string" ? item : "#"}
+                alt={`${productData.name} image`}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col justify-center gap-14">
+          <div className="p-2 max-w-xs h-min">
+            <p className="text-sm">
+              {capitalizeFirstLetter(productData.description || "")}
+            </p>
+          </div>
+          <p className="text-2xl">${productData.price}</p>
+          {attributes.map((item: IAttributes) => (
+            <div key={item.id}>
+              <RadioGroup
+                name={item.name}
+                onValueChange={(e) => onChange(e, item.name)}>
+                <p className="font-bold">{item.name}</p>
+                <div className="flex gap-4">
+                  {item.children_values.map((ele: any) => (
+                    <label
+                      key={ele}
+                      htmlFor={ele}
+                      className="flex gap-2 items-center">
+                      <RadioGroupItem value={ele} id={ele} required />
+                      <p>{ele}</p>
+                    </label>
+                  ))}
+                </div>
+              </RadioGroup>
+            </div>
+          ))}
+          <Button variant="outline" type="submit">
+            Add to Bag
+          </Button>
+        </div>
       </div>
     </form>
   );
